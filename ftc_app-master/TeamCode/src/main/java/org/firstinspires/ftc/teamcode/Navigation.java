f4b12df25759b6aced0f578eaa6de5cb02d172bd
ObviousThr33s <kfman18@gmail.com>
2017-11-25 16:06:57 -0800

    small changes

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Navigation.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Navigation.java
@@ -1,12 +1,17 @@
 package org.firstinspires.ftc.teamcode;
-
+import org.firstinspires.ftc.teamcode.AdafruitIMU.*;
 /**
  * Created by James on 11/13/2017.
  */
 
+
 public class Navigation {
     public double DistanceTraveled(){
-        //Impliment the acceleration formula for distance, import acceleration and times that against time.
-        return 0;
+        /**Impliment the acceleration formula for distance, import acceleration and times that against time.
+        double velocity = new AdafruitIMU().GetVelocity();
+        double acceleration = new AdafruitIMU().GetAcceleration();
+
+        return velocity;
+        return acceleration;*/
     }
 }

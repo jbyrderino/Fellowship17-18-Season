736fb43ca22eee4013a320c1aa60617e2c6b589b
ObviousThr33s <kfman18@gmail.com>
2018-01-19 20:05:36 -0800

    tabFixes

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KeithCarriage.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KeithCarriage.java
@@ -12,11 +12,11 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class KeithCarriage extends Carriage {
 
-    DcMotor slideMotor = null;
-    DcMotor flipMotor = null;
-    Servo lServo = null;
-    Servo rServo = null;
-    Telemetry tl;
+	DcMotor slideMotor = null;
+	DcMotor flipMotor = null;
+	Servo lServo = null;
+	Servo rServo = null;
+	Telemetry tl;
 
 //    KeithCarriage(DcMotor cM, DcMotor fM, Servo lS, Servo rS) {
 //        slideMotor = cM;
@@ -33,55 +33,55 @@ public class KeithCarriage extends Carriage {
 //        rServo.setPosition(0.0);
 //    }
 
-    public KeithCarriage(HardwareMap hwMap, Telemetry telemetry, String SMLabel, String FMLabel, String LSLabel, String RSLabel) {
-        tl = telemetry;
-        slideMotor = hwMap.get(DcMotor.class, SMLabel);
-        flipMotor = hwMap.get(DcMotor.class, FMLabel);
-        lServo = hwMap.get(Servo.class, LSLabel);
-        rServo = hwMap.get(Servo.class, RSLabel);
-    }
+	public KeithCarriage(HardwareMap hwMap, Telemetry telemetry, String SMLabel, String FMLabel, String LSLabel, String RSLabel) {
+		tl = telemetry;
+		slideMotor = hwMap.get(DcMotor.class, SMLabel);
+		flipMotor = hwMap.get(DcMotor.class, FMLabel);
+		lServo = hwMap.get(Servo.class, LSLabel);
+		rServo = hwMap.get(Servo.class, RSLabel);
+	}
 
-    public static final int LEFT = -1;
-    public static final int CENTER = 0;
-    public static final int RIGHT = 1;
-    public static final int DIS = 100;
+	public static final int LEFT = -1;
+	public static final int CENTER = 0;
+	public static final int RIGHT = 1;
+	public static final int DIS = 100;
 
-    public void setState(int state) {
-        slideMotor.setPower(Integer.signum(state * DIS - slideMotor.getCurrentPosition()));
-        while (Math.abs(slideMotor.getCurrentPosition() - state * DIS) > 10) {
-            //wait until finish
-        }
-        slideMotor.setPower(0.0);
-    }
+	public void setState(int state) {
+		slideMotor.setPower(Integer.signum(state * DIS - slideMotor.getCurrentPosition()));
+		while (Math.abs(slideMotor.getCurrentPosition() - state * DIS) > 10) {
+			//wait until finish
+		}
+		slideMotor.setPower(0.0);
+	}
 
-    private boolean inRange(int target, int value) {
-        return Math.abs(target - value) < 10;
-    }
+	private boolean inRange(int target, int value) {
+		return Math.abs(target - value) < 10;
+	}
 
-    boolean currentState;
-    static final int UP = 100;
-    static final int DOWN = 0;
+	boolean currentState;
+	static final int UP = 100;
+	static final int DOWN = 0;
 
-    public void flipToggle() {
-        flipMotor.setPower(currentState ? 1.0 : -1.0);
-        while ((currentState ? UP : DOWN) - flipMotor.getCurrentPosition() > 5) {
-            //wait until finish
-        }
-        currentState = !currentState;
-        flipMotor.setPower(0);
-    }
+	public void flipToggle() {
+		flipMotor.setPower(currentState ? 1.0 : -1.0);
+		while ((currentState ? UP : DOWN) - flipMotor.getCurrentPosition() > 5) {
+			//wait until finish
+		}
+		currentState = !currentState;
+		flipMotor.setPower(0);
+	}
 
-    static boolean holded;
-    static final double hold = 0.9;
-    static final double release = 0.1;
+	static boolean holded;
+	static final double hold = 0.9;
+	static final double release = 0.1;
 
-    public void holderToggle(boolean side) {
-        if (side) {
-            lServo.setPosition(holded ? release : hold);
-        } else {
-            lServo.setPosition(holded ? release : hold);
-        }
-    }
+	public void holderToggle(boolean side) {
+		if (side) {
+			lServo.setPosition(holded ? release : hold);
+		} else {
+			lServo.setPosition(holded ? release : hold);
+		}
+	}
 
 
 }
--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KeithElevator.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KeithElevator.java
@@ -6,45 +6,45 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.HardwareDevice;
 
 public class KeithElevator{
-    private DcMotor ElevatorMotor = null;
-    private Servo ElevatorServo = null;
-    private double motorPower;
+	private DcMotor ElevatorMotor = null;
+	private Servo ElevatorServo = null;
+	private double motorPower;
 
-    KeithElevator (HardwareMap hwmap, double MotorPower) {
-        ElevatorMotor = hwmap.get(DcMotor.class, "ElevatorMotor");
-        ElevatorServo = hwmap.get(Servo.class, "ElevatorServo");
-        ElevatorInit(MotorPower);
-    }
+	KeithElevator (HardwareMap hwmap, double MotorPower) {
+		ElevatorMotor = hwmap.get(DcMotor.class, "ElevatorMotor");
+		ElevatorServo = hwmap.get(Servo.class, "ElevatorServo");
+		ElevatorInit(MotorPower);
+	}
 
-    public void elevatorStart(double Pwr) {
-        ElevatorMotor.setPower(Pwr);
-    }
+	public void elevatorStart(double Pwr) {
+		ElevatorMotor.setPower(Pwr);
+	}
 
-    public void elevatorStop() {
-        ElevatorMotor.setPower(0.0);
-    }
+	public void elevatorStop() {
+		ElevatorMotor.setPower(0.0);
+	}
 
-    public void kickerInit(){
-        ElevatorServo.scaleRange(0.0, 1.0);
-        ElevatorServo.setPosition(0.0);
-    }
+	public void kickerInit(){
+		ElevatorServo.scaleRange(0.0, 1.0);
+		ElevatorServo.setPosition(0.0);
+	}
 
-    public void kickerSetPosition(double Pos) {
-        ElevatorServo.setPosition(Pos);
-    }
+	public void kickerSetPosition(double Pos) {
+		ElevatorServo.setPosition(Pos);
+	}
 
-    public void kickerReset() {
-        ElevatorServo.setPosition(0.0);
-    }
+	public void kickerReset() {
+		ElevatorServo.setPosition(0.0);
+	}
 
-    public void kickerKick() {
-        kickerReset();
-        kickerSetPosition(1.0);
-        kickerReset();
-    }
+	public void kickerKick() {
+		kickerReset();
+		kickerSetPosition(1.0);
+		kickerReset();
+	}
 
-    public void ElevatorInit(double mp) {
-        motorPower = mp;
-        kickerInit();
-    }
+	public void ElevatorInit(double mp) {
+		motorPower = mp;
+		kickerInit();
+	}
 }

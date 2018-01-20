31d27470689c058e8d364d10add5abfaf0259b8c
Alpha-kun <CharlieLiuAlpha@gmail.com>
2018-01-19 18:24:20 -0800

    Carriage Test Program
    This is going to be a program that tests Carriage System.

---
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CarriageTest.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Created by 28761 on 1/16/2018.
+ */
+
+public class CarriageTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
